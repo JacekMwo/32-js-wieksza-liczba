b9416ba832925d9617619192487251ee8319787e
JacekMwo <jckbanach@gmail.com>
2023-05-08 12:00:11 +0200

    32-js-wieksza-liczba

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let num1 = 2;
+let num2 = 5;
+
+function ktoraToWiekszaLiczba(a, b) {
+    if (a > b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+let wiekszaLiczba = ktoraToWiekszaLiczba(num1, num2);
+console.log(wiekszaLiczba);
